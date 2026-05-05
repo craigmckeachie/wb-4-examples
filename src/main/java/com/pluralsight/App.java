c11c1572999c7ecd1b4122bebca321ab0fc2c432
Craig McKeachie <craigdmckeachie@gmail.com>
2026-05-05 10:42:38 -0400

    add overloaded constructors and more people

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -9,8 +9,26 @@ public class App {
         person.setFirstName("Michael");
         person.setLastName("Jordan");
         person.setBirthDate(LocalDate.of(1963, 2, 17));
+        person.setNice(true);
 
+        System.out.println(person.toString());
+        //OR
         System.out.println(person);
+
+
+        System.out.println(person.getFirstName());
+        System.out.println(person.getLastName());
+        System.out.println(person.getBirthDate());
+        System.out.println(person.isNice());
+
+
+        Person person2 = new Person("Yuliia", "Jones");
+        System.out.println(person2);
+
+        Person person3 = new Person("Luka","Doncic",LocalDate.of(1999, 2, 28),false);
+        System.out.println(person3);
+
+
 
 
     }
--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -8,12 +8,25 @@ public class Person {
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
+    private boolean nice;
 
     //constructor(s)
     //default parameterless constructor
-//    public Person() {
-//    }
+    public Person() {
+    }
 
+
+    public Person(String firstName, String lastName, LocalDate birthDate, boolean nice) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.nice = nice;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     //getters and setters
     public void setFirstName(String firstName) {
@@ -26,6 +39,26 @@ public class Person {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public void setNice(boolean nice) {
+        this.nice = nice;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public boolean isNice() {
+        return nice;
     }
 
     //methods
